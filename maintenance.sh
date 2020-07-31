3c29f954d0f8f185fec535c01e3610c2b349567a
Schlomo Schapiro <schlomo+git@schapiro.org>
2020-07-31 10:55:21 +0200

    Also for employees and don't touch nested groups

--- maintenance.sh
+++ maintenance.sh
@@ -8,7 +8,8 @@ if [ ! -x "$gam" ] ; then
     exit 98
 fi
 
-# Sync teachers group with OU
+# Sync Mitarbeiter / Lehrer group with OU
 # https://github.com/taers232c/GAMADV-XTD3/wiki/Collections-of-Users#users-directly-in-the-organization-unit-orgunititem
 # https://github.com/taers232c/GAMADV-XTD3/wiki/Groups-Membership#synchronize-members-in-a-group
-$gam update group allelehrer sync ou /Mitarbeiter/Lehrer
+$gam update group allelehrer sync usersonly notsuspended ou /Mitarbeiter/Lehrer
+$gam update group mitarbeiter sync usersonly notsuspended ou /Mitarbeiter
