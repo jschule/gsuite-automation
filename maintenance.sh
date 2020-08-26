96dbe3d8b73209628a4bc942c2d8920da1e96e4a
Schlomo Schapiro <schlomo@schapiro.org>
2020-08-26 11:11:55 +0000

    Update master data with new student/teacher lists

--- maintenance.sh
+++ maintenance.sh
@@ -13,20 +13,39 @@ source config.sh
 test "$MASTERSHEET"
 test "$MASTERUSER"
 
-# Sync Mitarbeiter / Lehrer group with OU
+function make_gam_sheet_update_json {
+    # read list from stdin and generate GAM Sheet update json for named range
+    range="$1"; shift
+    jq -n --slurpfile data <(jq -R '[.]') '[{"range":"'"$range"'","majorDimension":"ROWS"}] | .[0].values=$data'
+
+}
+
+function info {
+    # print info headergit 
+    echo -e "*\n*\n*\n***  $*  ***\n*"
+}
+
+info Sync Mitarbeiter / Lehrer group with OU
 # https://github.com/taers232c/GAMADV-XTD3/wiki/Collections-of-Users#users-directly-in-the-organization-unit-orgunititem
 # https://github.com/taers232c/GAMADV-XTD3/wiki/Groups-Membership#synchronize-members-in-a-group
 $gam update group allelehrer sync usersonly notsuspended ou /Mitarbeiter/Lehrer
 $gam update group mitarbeiter sync usersonly notsuspended ou /Mitarbeiter
 
-# Sync all student groups from master sheet data
+info Sync all student groups from master sheet data
 $gam loop gsheet "$MASTERUSER" "$MASTERSHEET" "gam Schülerverteiler" \
 	gam update group "~Gruppe" \
 	sync member usersonly users "~Mitglieder"
 
-# Sync all parent groups from master sheet data
+info Sync all parent groups from master sheet data
 $gam loop gsheet "$MASTERUSER" "$MASTERSHEET" "gam Elternverteiler" \
 	gam update group "~Gruppe" \
 	sync member usersonly users "~Mitglieder"
 
+info Update Mitarbeiter and Schüler in Master Data
+$gam ou_and_children Mitarbeiter print user | $gam user "$MASTERUSER" \
+    update sheetrange "$MASTERSHEET" \
+    json file <(make_gam_sheet_update_json Mitarbeiter)
 
+$gam ou_and_children Schüler print user | $gam user "$MASTERUSER" \
+    update sheetrange "$MASTERSHEET" \
+    json file <(make_gam_sheet_update_json Schüler)
